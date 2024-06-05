58ddff54d63b2888a7b4038980fb9710c13d29b7
Romeo Basser <99049952+rb990@users.noreply.github.com>
2024-06-05 15:01:37 +0100

    Update tempSensor.py
    Added docstring

--- code/tempSensor.py
+++ code/tempSensor.py
@@ -5,16 +5,23 @@ from dht import DHT22
 dht22_sensor = DHT22(Pin(5, Pin.IN, Pin.PULL_UP))
 
 def tempMeasure():
+    """
+    Communicates with DHT22 chip and returns the temperature
+    """
     dht22_sensor.measure()
     temp = dht22_sensor.temperature()
     return temp
 
 def tempLoop():
+    """
+    Simple loop for testing; returns the temperature and humidity readings from the DHT22 chip.
+    """
     i=0
     while True:
         sleep(2)
         i+=1
-        temp, humi = tempMeasure()
+        dht22_sensor.measure()
+        temp, humi = dht22_sensor.temperature(), dht22_sensor.humidity()
         print('Reading {}, Temperature: {:0.1f}C, Humidity: {:0.1f}%'.format(i, temp, humi))
 
 
