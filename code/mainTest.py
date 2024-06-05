fe895b94bc6ffe04bc3c43e16093a07c77843209
Romeo Basser <99049952+rb990@users.noreply.github.com>
2024-06-05 14:56:12 +0100

    Update mainTest.py

--- code/mainTest.py
+++ code/mainTest.py
@@ -6,17 +6,6 @@ from tempSensor import tempMeasure
 from buzzer import buzz
 from constants import updateJson, dumpJson
 
-
-"""
-This is a variant of main.py used for testing the functions.
-
-Differences:
-Uses a tdelta/polling rate of 1 second, as opposed to 5 minutes. Allows for quick testing of buzzer.
-Greatly accelerates heat loss, by a factor of 3600, to model 1 hour passing every second.
-Upon bootup, will always choose revertQ to be half of the cool life; around 54 hours at 21.5C.
-"""
-
-
 class SoftSwitch():
     """Switch Class
  
@@ -28,7 +17,7 @@ class SoftSwitch():
     clicking the button down, then clicking it again quickly, or adjusting the
     sensitivity in self.sensitivity.
     """
-    def __init__(self, pin, constants: dict, checks=3, check_period=100):
+    def __init__(self, pin, constants: dict, tempMeasure, changeDisplay, checks=3, check_period=100):
         self.pin = pin
         self.pin.irq(handler=self._switch_change,
                      trigger= machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING)
@@ -40,7 +29,7 @@ class SoftSwitch():
         self.debounce_checks = 0
         self.checks = checks
         self.check_period = check_period
-        self.sensitivity = 0.75
+        self.sensitivity = 0.35
         self.reverted = False
         self.timeFinished = False
         self.previousTime = False
@@ -81,24 +70,25 @@ class SoftSwitch():
                     
                 self.currentTime = time()
                 if not self.previousTime:
-                    self.previousTime = self.currentTime
+                    self.previousTime = self.currentTime - 1000 #Makes sure the first click isn't a double click
                 dummyQ = False
-                if self.currentTime - self.previousTime < self.sensitivity:
+                if self.currentTime - self.previousTime < self.sensitivity: # Check for a double click with sensitivity _
                     print("Double click detected")
-                    if self.timeFinished:
-                        dummyQ = constants["Qtotal"]
+                    if self.reverted == False: # Makes sure it only reverts once
+                        self.reverted = True 
+                        print("Reverting Q to", constants["revertQ"])
+                        dummyQ = max(0, constants["revertQ"]) # Makes sure that it doesn't revert the cool life to a negative number; if 0 then can be reset
+                        changeDisplay(revert = True)
                     else:
-                        if self.reverted == False:
-                            self.reverted = True
-                            print("Reverting Q to", constants["revertQ"])
-                            dummyQ = max(0, constants["revertQ"])
-                            displayStrings("Reverting to previous Q", None)
-                            sleep(1)
-                        else:
-                            print("Already reverted Q")
+                        changeDisplay(reset = True)
+                        dummyQ = constants["Qtotal"]  
                 if dummyQ:     
                     global Qleft
                     Qleft = dummyQ
+                    temp = tempMeasure()
+                    heatLossRate = (temp) / constants["Rth"]
+                    timeRemaining = dummyQ / (heatLossRate * (60*60))
+                    displayTime(timeRemaining, None)
                 self.previousTime = self.currentTime
 
  
@@ -116,81 +106,66 @@ class SoftSwitch():
             self._start_debounce_timer()
 
 #initialise extra components
-buzzer = Pin(14, Pin.OUT, Pin.PULL_DOWN) # Buzzer is connected between GPIO pin 14 (board pin 19), and ground. Polarity does matter
-bottomButton = Pin(15, Pin.IN, Pin.PULL_UP) # Bottom button is connected between GPIO pin 15 (board pin 20) and ground)
-doorSwitch = Pin(16, Pin.IN, Pin.PULL_UP) # Door switch is connected between GPIO pin 16 (board pin 21), and ground
+buzzer = Pin(10, Pin.OUT, Pin.PULL_DOWN) # Buzzer is connected between GPIO pin 14 (board pin 19), and ground. Polarity does matter
+pushButton = Pin(18, Pin.IN, Pin.PULL_UP) # Push button is connected between GPIO pin 15 (board pin 20) and ground)
+doorSwitch = Pin(14, Pin.IN, Pin.PULL_UP) # Door switch is connected between GPIO pin 16 (board pin 21), and ground
+constants = updateJson('constants.json') # Import all the relevant constants
+Qleft = constants["Qtotal"] # Initialises heat energy.
 endBuzz = False
 
-
-constants = updateJson('constants.json') # Import all the relevant constants
-Qleft = constants["Qtotal"] # Initialises the heat energy
-
-
-constants["tdelta"] = 1 # 1 second polling rate
-constants["revertQ"] = 434200 # Half of the original Q
-# ^ used for testing.
-
+constants["tdelta"] = 1 #used for testing
 
 def wait_for_start(x):
     """
-    An interrupt raised at the start, caused by the button at the bottom of the module.
+    An interrupt raised at the start, caused by the push button.
     This commences the rest of the code.
     """
-    bottomButton.irq(trigger=0) # Disable the interrupt to prevent further recursions
+    pushButton.irq(trigger=0) # Disable the interrupt to prevent further recursions
     global started
     started = True
     return
 
-try:
-    bottomButton.irq(trigger=Pin.IRQ_RISING, handler=wait_for_start) # Set the interrupt to the bottom button
-    print("Waiting to start; push the bottom button or insert the module to start the timer")
-    # Waits for the button (at the bottom of the module) to be pushed to start the programme
-    started = False
-    waitForStart()
-    while started is False:
-        sleep(1)
-        continue
-    print("starting")
-    bottomButton = SoftSwitch(Pin(15, machine.Pin.IN, machine.Pin.PULL_UP), constants) # THIS REQUIRES A TECHNICAL PRESS, PUSH THE BUTTON THEN PUSH AGAIN IN QUICK SUCCESSION, OR A QUICK TRIPLE CLICK
-    count = 0 # A counter for the number of loops the door has been left open
+#try:
+pushButton.irq(trigger=Pin.IRQ_RISING, handler=wait_for_start) # Attach the interrupt to the push button
+print("Waiting to start; push the button or insert the module to start the timer")
+# Waits for the button to be pushed to start the programme
+started = False
+waitForStart()
+while started is False:
+    sleep(1)
+    continue
+print("starting")
+pushButton = SoftSwitch(Pin(18, machine.Pin.IN, machine.Pin.PULL_UP), constants, tempMeasure, changeDisplay) # THIS REQUIRES A TECHNICAL PRESS, PUSH THE BUTTON THEN PUSH AGAIN IN QUICK SUCCESSION, OR A QUICK TRIPLE CLICK
+count = 0 # A counter for the number of loops the door has been left open
+lastTime = time()
+while True:
+    timeDiff = (time() - lastTime) # Used to calculate the time difference over which heat was lost
+    temp = tempMeasure()
+    heatLossRate = (temp) / constants["Rth"] # (W), note the temp is actually a temperature difference, but since the ice pack core is 0C, the temperature in Celcius alone works
+    QLoss = heatLossRate * (timeDiff * 60*60*20) # (Heat energy, J, lost to surroundings). 20 HOURS PER TDELTA FOR TESTING
+    Qleft -= QLoss #Calculate remaining Q
+    if Qleft <=0:
+        pushButton.endTimer()
+        if endBuzz == False:
+            buzz(buzzer, 2)
+            endBuzz = True
+    timeRemaining = Qleft / (heatLossRate * (60*60)) # in hours
+    print(timeRemaining) # For testing, note that at 1s intervals, approx time loss in hrs is 0.00028 (at constant T)
+    displayTime(timeRemaining, False)                                                                         
     lastTime = time()
-    while True:
-        timeDiff = (time() - lastTime) # Used to calculate the time difference over which heat was lost
-        temp, humi = tempMeasure()
-        heatLossRate = (temp) / constants["Rth"] # (W), note the temp is actually a temperature difference, but since the ice pack core is 0C, the temperature in Celcius alone works
-        QLoss = heatLossRate * timeDiff * 3600 # Heat energy, J, lost to surroundings, with scaling factor to accommodate testing
-        Qleft -= QLoss #Calculate remaining Q
-        if Qleft <=0:
-            bottomButton.endTimer()
-            if endBuzz == False:
-                buzz(buzzer, 2)
-                endBuzz = True
-        timeRemaining = Qleft / (heatLossRate * (60*60)) # in hours
-        print(timeRemaining) # For testing, note that at 1s intervals, approx time loss in hrs is 0.00028 (at constant T)
-        displayTime(timeRemaining, False)                                                                         
-        lastTime = time()
-        currentSwitch = doorSwitch.value()
-        if currentSwitch == 1: # Checks if the door is open. Waits another iteration: if the door is still open, buzz
-            count+=1
-            if count >=2:
-                #count = 0
-                buzz(buzzer, 1)
-        else:
-            count = 0
-        sleep(constants["tdelta"]) # Polling rate of tdelta, default 5 mins
+    currentSwitch = doorSwitch.value()
+    if currentSwitch == 1: # Checks if the door is open. Waits another iteration: if the door is still open, buzz
+        count+=1
+        if count >=2:
+            #count = 0
+            buzz(buzzer, 1)
+    else:
+        count = 0
+    sleep(constants["tdelta"]) # Polling rate of tdelta, default 5 mins
     
-except:
-    print("Error raised somewhere / KBDInterrupt")
-    turnoff()
-    constants["revertQ"] = Qleft
-    constants["revertTime"] = time()
-    dumpJson("constants.json", constants)
-
-    
-
-
-
-
-
-
-
+#except:
+print("Error raised somewhere / KBDInterrupt.")
+turnoff()
+constants["revertQ"] = Qleft
+constants["revertTime"] = time()
+dumpJson("constants.json", constants)
